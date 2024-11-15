692179bb2ac9813d04000b44874c6d506d741c88
FranLSan <francisco.lsanchez0798@hotmail.com>
2024-11-15 17:00:45 -0600

    Cascaron

---
+++ src/mvc_prueba/MVC_Prueba.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package mvc_prueba;
+
+/**
+ *
+ * @author Desarrollador3
+ */
+public class MVC_Prueba {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
